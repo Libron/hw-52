38b01aa04218ea7f0e6dbb9da5ae5fb5a5f4277d
Urmat Bekboev <bek.urmat@gmail.com>
2018-12-18 18:11:31 +0600

    Added render() of Loto component, changed title of index file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Loto from './components/Loto';
 import './App.css';
 
 class App extends Component {
+  state = {
+        numbers: [1, 2, 3, 4, 5]
+  };
+
+  getRandomNumbers = (size = 5) => {
+        const numbers = [];
+        for (let i = 0; i < size; i++) {
+            while(true) {
+                let randomNumber = Math.floor(Math.random() * 32) + 5;
+                if (numbers.indexOf(randomNumber) === -1) {
+                    numbers.push(randomNumber);
+                    break;
+                }
+                continue;
+            }
+        }
+        return numbers.sort(function(a, b){return a-b});
+  };
+
+
+    changeNumbers = () => {
+      const newNumbers = this.getRandomNumbers();
+      this.setState({numbers: newNumbers});
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Loto numbers={this.state.numbers} />
+          <button className="btn" onClick={this.changeNumbers}>New Numbers</button>
       </div>
     );
   }
