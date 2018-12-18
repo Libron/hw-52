5eaed695105efd31ab7f75ec094b07d368f60d97
Urmat Bekboev <bek.urmat@gmail.com>
2018-12-18 17:28:30 +0600

    Added new Loto component Loto.js and Loto.css

---
+++ src/components/Loto.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './Loto.css';
+
+class Loto extends Component {
+    render() {
+        console.log(this.props);
+        return (
+            <div className="lotoreya">
+                <ul>
+                    <li>{this.props.numbers[0]}</li>
+                    <li>{this.props.numbers[1]}</li>
+                    <li>{this.props.numbers[2]}</li>
+                    <li>{this.props.numbers[3]}</li>
+                    <li>{this.props.numbers[4]}</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Loto;
